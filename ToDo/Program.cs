25c8e42b886997abb460efdb390ee788b8b0408c
“pablobelmonteok-fallo” <pablobelmontecrack@gmail.com>
2026-06-08 16:48:49 -0300

    punto 1 cargado

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,31 +1,99 @@
-﻿Console.WriteLine("Hello, World!");
-// lista de objetos int
+﻿Console.WriteLine("Cantidad de tareas: ");
+int.TryParse(Console.ReadLine(), out int valor) ;
+
 // crear una lista vacía
-
 List<Tarea>tareasPendientes = new List<Tarea>();
-tareasPendientes.Add(new Tarea { TareaID = 1, Descripcion = "pendiente", Duracion = 2});
-tareasPendientes.Add(new Tarea { TareaID = 2, Descripcion = "pendiente", Duracion = 3});
-
-for (int i = 0; i < Cantmax; i++)
+for (int i = 0; i < valor; i++)
 {
     Tarea tarea = new Tarea ();
-     tarea.Descripcion = "pediente";
-     tarea.Duracion = 2;
+     Console.WriteLine($"Ingrese descripción de la tarea {i}: ");
+    tarea.Descripcion = Console.ReadLine();
+     tarea.Duracion = 13;
      tarea.TareaID = i;
+     tareasPendientes.Add(tarea);
 }
-// inicializar con valores
-//List<int> numeros = new List<int>() { 10, 20, 30 };
-   // acceso por índice (base 0)
-// "Manzana"
 
-// recorrer con foreach
+// recorrer la lista con foreach
 foreach (Tarea f in tareasPendientes)
 {
+ Console.WriteLine("Lista de Tareas");
    Console.WriteLine(f.TareaID);
    Console.WriteLine(f.Descripcion);
    Console.WriteLine(f.Duracion);
    Console.WriteLine("\n");
 }
 
-// cantidad de elementos
-    // 3
+List<Tarea>tareasRealizadas = new List<Tarea>();
+foreach (Tarea f in tareasPendientes)
+{
+   if(f.Descripcion == "Realizadas")
+    { 
+        
+        tareasRealizadas.Add(f);
+    }
+}
+
+// elimina el bloque de tareas pendientes 
+tareasPendientes.RemoveAll(f => f.Descripcion == "Realizadas");
+foreach (Tarea T in tareasRealizadas)
+{
+   Console.WriteLine("Lista de Tareas Realizadas");
+   Console.WriteLine(T.TareaID);
+   Console.WriteLine(T.Descripcion);
+   Console.WriteLine(T.Duracion);
+   Console.WriteLine("\n");
+}
+
+Console.WriteLine("---------------------- Menu Principal ----------------------");
+Console.WriteLine("Presione 1: Buscar tareas pendientes");
+Console.WriteLine("Presione 2: Buscar tareas realizadas");
+Console.WriteLine("Presione 3: Buscar por ID de la tarea");
+int.TryParse(Console.ReadLine(), out int buscar) ;
+
+if (buscar == 1)
+{
+ foreach (Tarea f in tareasPendientes)
+{
+ Console.WriteLine("Lista de Tareas");
+   Console.WriteLine(f.TareaID);
+   Console.WriteLine(f.Descripcion);
+   Console.WriteLine(f.Duracion);
+   Console.WriteLine("\n");
+}
+}
+if (buscar == 2){
+        foreach (Tarea T in tareasRealizadas)
+{
+   Console.WriteLine("Lista de Tareas Realizadas");
+   Console.WriteLine(T.TareaID);
+   Console.WriteLine(T.Descripcion);
+   Console.WriteLine(T.Duracion);
+   Console.WriteLine("\n");
+}
+}
+if (buscar == 3)
+{
+ Console.WriteLine("ID que requiera encontrar: ");
+int.TryParse(Console.ReadLine(), out int id) ;
+      foreach (Tarea T in tareasRealizadas)
+{
+    if(T.TareaID == id){
+   Console.WriteLine("encontrado en Tareas Realizadas");
+   Console.WriteLine(T.TareaID);
+   Console.WriteLine(T.Descripcion);
+   Console.WriteLine(T.Duracion);
+   Console.WriteLine("\n");
+   }
+}
+      foreach (Tarea T in tareasPendientes)
+{
+    if(T.TareaID == id){
+   Console.WriteLine("encontrado en Tareas Pealizadas");
+   Console.WriteLine(T.TareaID);
+   Console.WriteLine(T.Descripcion);
+   Console.WriteLine(T.Duracion);
+   Console.WriteLine("\n");
+   }
+}
+}
+    
--- ToDo/obj/Debug/net10.0/ToDo.AssemblyInfo.cs
+++ ToDo/obj/Debug/net10.0/ToDo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26fb79cd7186c2cd19ca4070988b11670ee4b522")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
